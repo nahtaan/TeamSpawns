6b6fbe4fac54542b8bde20065f91a7821e568169
Nathan <nathanjohnbateson@gmail.com>
2024-11-12 16:56:44 +0000

    Additional methods for the TeamManager and new configuration options.

--- src/main/java/dev/nahtan/teamSpawns/data/TeamManager.java
+++ src/main/java/dev/nahtan/teamSpawns/data/TeamManager.java
@@ -1,13 +1,14 @@
 package dev.nahtan.teamSpawns.data;
 
 import dev.nahtan.teamSpawns.TeamSpawns;
-import org.bukkit.NamespacedKey;
+import org.bukkit.*;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.awt.*;
+import java.awt.Color;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -48,8 +49,11 @@ public class TeamManager {
             double x = teamSection.getDouble("x");
             double y = teamSection.getDouble("y");
             double z = teamSection.getDouble("z");
+            double yaw = teamSection.getDouble("yaw");
+            double pitch = teamSection.getDouble("pitch");
+            String world = teamSection.getString("world");
 
-            if(!validateConfig(key, name, description, colour)) {
+            if(!validateConfig(key, name, description, colour, world)) {
                 continue;
             }
 
@@ -57,13 +61,13 @@ public class TeamManager {
                 TeamSpawns.LOGGER.warning("A duplicate team name has been found: '" + name + "'. The second instance of this team will be ignored.");
                 continue;
             }
-            TeamInfo info = new TeamInfo(name, description, colour, x, y, z);
+            TeamInfo info = new TeamInfo(name, description, colour, x, y, z, (float) yaw, (float) pitch, world);
             teamSpawns.put(name, info);
         }
         return !teamSpawns.isEmpty();
     }
 
-    private boolean validateConfig(String key, String name, String description, String colour) {
+    private boolean validateConfig(String key, String name, String description, String colour, String world) {
         boolean valid = true;
         if(name == null) {
             TeamSpawns.LOGGER.warning("Name has not been provided for team '" + key + "'. This team will be ignored.");
@@ -84,6 +88,10 @@ public class TeamManager {
                 valid = false;
             }
         }
+        if(world == null) {
+            TeamSpawns.LOGGER.warning("World has not been provided for team '" + key + "'. This team will be ignored.");
+            valid = false;
+        }
         return valid;
     }
 
@@ -95,6 +103,19 @@ public class TeamManager {
         return teamSpawns.containsKey(teamName);
     }
 
+    public Location getTeamSpawnLoc(String teamName) {
+        if(!doesTeamExist(teamName)) {
+            return null;
+        }
+        TeamInfo info = teamSpawns.get(teamName);
+        World world = Bukkit.getWorld(info.world);
+        if(world == null) {
+            TeamSpawns.LOGGER.severe("Could not find spawn world for team '" + info.name + "', world: '" + info.world + "'. Defaulting to first world that can be found.");
+            world = Bukkit.getWorlds().getFirst();
+        }
+        return new Location(world, info.x, info.y, info.z, info.yaw, info.pitch);
+    }
+
     private static class TeamInfo {
         String name;
         String description;
@@ -102,14 +123,20 @@ public class TeamManager {
         double x;
         double y;
         double z;
+        float yaw;
+        float pitch;
+        String world;
 
-        public TeamInfo(String name, String description, String colourCode, double x, double y, double z) {
+        public TeamInfo(String name, String description, String colourCode, double x, double y, double z, float yaw, float pitch, String world) {
             this.name = name;
             this.description = description;
             this.colourCode = colourCode;
             this.x = x;
             this.y = y;
             this.z = z;
+            this.yaw = yaw;
+            this.pitch = pitch;
+            this.world = world;
         }
     }
 }
